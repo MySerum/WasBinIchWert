ff709a00874c831ffecf4ebee1d50db1d6448634
MySerum <155495334+MySerum@users.noreply.github.com>
2026-08-31 15:49:34 +0200

    Load PDF export module

--- brand-runtime.js
+++ brand-runtime.js
@@ -1,1 +1,2 @@
 document.querySelector('.brand-icon')?.setAttribute('src','./brand-mark.svg');
+import('./pdf-export.js?v=822').catch(err=>console.error('PDF export konnte nicht geladen werden',err));
